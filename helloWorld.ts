7a2f18b7646135faa1b61522546883d1eec3f0ed
Oleg Selivantsev <o.selivancev@gmail.com>
2020-01-17 02:53:19 +0300

    ts update

--- helloWorld.ts
+++ helloWorld.ts
@@ -1,2 +1,24 @@
 let message: string = "Hello World";
-console.log(message);
+console.log(message + 1);
+
+let counterrrr: number = 2;
+counterrrr = counterrrr + 100;
+console.log(counterrrr);
+
+function foo(val: string) {
+  const _val = val + "**";
+  setTimeout(() => {
+    console.log(_val);
+  }, 5000);
+}
+
+foo(message + counterrrr);
+
+let data: Array<string> = ["a", "b", "c"];
+console.log("data", data);
+
+function arrayHandler() {
+  data.forEach((item, i) => console.log(i, " ", item));
+}
+
+arrayHandler();
